a12f59ad172d01f275dd611136678b41c68d0566
yrazafindrasata <yrazafindrasata@yacast.fr>
2020-03-17 18:36:28 +0100

    fix path

--- src/Api/TumblrApi.php
+++ src/Api/TumblrApi.php
@@ -14,10 +14,10 @@ class TumblrApi
             $data['body'] = $content['description'];
         }
         if($content['type'] == "photo"){
-            $data['source'] =  "http://127.0.0.1/uploads/files/".$content['file'];
+            $data['source'] =  "https://www.myprojectsdev.fr/uploads/files/".$content['file'];
         }
         if($content['type'] == "video"){
-            $data['data'] = "http://127.0.0.1/uploads/files/".$content['file'];
+            $data['data'] = "https://www.myprojectsdev.fr/uploads/files/".$content['file'];
         }
         if($content['type'] == "link"){
             $data['title'] = $content['title'];
